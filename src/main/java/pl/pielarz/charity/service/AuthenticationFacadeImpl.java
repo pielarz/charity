fa97654050176f78191e8d3239764979df7cb590
Michał Pielarz <pielarz.michal@gmail.com>
2020-05-16 16:19:17 +0200

    facade for authentication

---
+++ src/main/java/pl/pielarz/charity/service/AuthenticationFacadeImpl.java
@@ -0,0 +1,13 @@
+package pl.pielarz.charity.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacadeImpl implements AuthenticationFacade{
+    @Override
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
